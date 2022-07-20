7ab6af3215828d54324ec8432880ae3ce4a3d6fa
Felipe <felipe@toke.com>
2022-07-20 01:13:47 -0300

    Queue Mini View remoedel

--- lib/features/queue/queue_mini_view.dart
+++ lib/features/queue/queue_mini_view.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_cdg_karaoke_player/config/strings.dart';
 import 'package:flutter_cdg_karaoke_player/model/song_queue_item.dart';
 import 'package:flutter_cdg_karaoke_player/service/queue_controller.dart';
 
@@ -16,37 +17,45 @@ class QueueMiniView extends StatelessWidget {
       child: Acrylic(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: StreamBuilder<Queue<SongQueueItem>>(
-            stream: queueController.queueStream,
-            builder: (context, snapshot) {
-              final queue = snapshot.data;
-              if (queue == null) {
-                return const Center(child: ProgressRing());
-              }
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(Strings.queue.tr, style: FluentTheme.of(context).typography.title),
+              Expanded(
+                child: StreamBuilder<Queue<SongQueueItem>>(
+                  stream: queueController.queueStream,
+                  builder: (context, snapshot) {
+                    final queue = snapshot.data;
+                    if (queue == null) {
+                      return const Center(child: ProgressRing());
+                    }
 
-              if (queue.isEmpty) {
-                return ListView(
-                  padding: const EdgeInsets.all(8.0),
-                  children: [
-                    Acrylic(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-                      tint: FluentTheme.of(context).disabledColor,
-                      child: const ListTile(title: Text('No songs in queue'), leading: Icon(FluentIcons.field_empty)),
-                    )
-                  ],
-                );
-              }
+                    if (queue.isEmpty) {
+                      return ListView(
+                        padding: const EdgeInsets.all(8.0),
+                        children: [
+                          Acrylic(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                            tint: FluentTheme.of(context).disabledColor,
+                            child: const ListTile(title: Text('No songs in queue'), leading: Icon(FluentIcons.field_empty)),
+                          )
+                        ],
+                      );
+                    }
 
-              return ListView.builder(
-                itemCount: queue.length,
-                itemBuilder: (context, index) => ListTile(
-                  title: Text(queue.elementAt(index).song.title),
-                  leading: Text(index.toString()),
-                  subtitle: Text(queue.elementAt(index).singer.name),
+                    return ListView.builder(
+                      itemCount: queue.length,
+                      itemBuilder: (context, index) => ListTile(
+                        title: Text(queue.elementAt(index).song.title),
+                        leading: Text(index.toString()),
+                        subtitle: Text(queue.elementAt(index).singer.name),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
+              ),
+            ],
           ),
         ),
       ),
